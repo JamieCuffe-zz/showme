98c1993174a2b2bd5b1e96ab9b46edc50977279b
Roopa Ramanujam <roopa.ramanujam22@gmail.com>
2018-04-08 15:56:12 -0400

    attempting to add certificate db

--- hello/models.py
+++ hello/models.py
@@ -3,13 +3,11 @@ from django.db import models
 # Create your models here.
 class Greeting(models.Model):
     when = models.DateTimeField('date created', auto_now_add=True)
-
-class Dreamreal(models.Model):
-
-   website = models.CharField(max_length = 50)
-   mail = models.CharField(max_length = 50)
-   name = models.CharField(max_length = 50)
-   phonenumber = models.IntegerField()
-
-   class Meta:
-      db_table = "dreamreal"
+    
+#NEW
+class Certificates(models.Model):
+	name = models.CharField(max_length=255)
+	prereqs = models.CharField(max_length=255)
+	deparmentHead = models.CharField(max_length=255)
+	class Meta:
+      db_table = "certificates"
--- hello/views.py
+++ hello/views.py
@@ -2,19 +2,32 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 from .models import Greeting
+from .models import Certificates
 
 # Create your views here.
 def index(request):
     # return HttpResponse('Hello from Python!')
-    return render(request, 'index.html')
+
+    #NEW
+    certificate = Certificates(
+    	name = "Applications of Computing", prereqs = "COS 126;COS 226;COS 217", 
+    	departmentHead = "Colleen Kenny"
+    	)
+
+    certificate.save()
+
+    certs = Certificates.objects.all()
+
+    #END NEW
+    return render(request, 'index.html', {'certificates' : certs})
 
 
 def db(request):
 
-    greeting = Greeting()
-    greeting.save()
+	greeting = Greeting()
+	greeting.save()
 
-    greetings = Greeting.objects.all()
+	greetings = Greeting.objects.all()
 
-    return render(request, 'db.html', {'greetings': greetings})
+	return render(request, 'db.html', {'greetings': greetings})
 
