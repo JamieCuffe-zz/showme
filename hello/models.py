35c3f86e87c31686bfd32966379dad0224eaaf70
Roopa Ramanujam <roopa.ramanujam22@gmail.com>
2018-04-20 09:45:10 -0400

    fixed error in settings.py (missing hello app) and deleted greeting/certs models

--- hello/models.py
+++ hello/models.py
@@ -1,18 +1,18 @@
 from django.db import models
 
 # Create your models here.
-class Greeting(models.Model):
-    when = models.DateTimeField('date created', auto_now_add=True)
+# class Greeting(models.Model):
+#     when = models.DateTimeField('date created', auto_now_add=True)
 
 #NEW
 
-class Certs(models.Model):
+# class Certs(models.Model):
 
-   name = models.CharField(max_length = 50)
-   prereqs = models.CharField(max_length = 50)
+#    name = models.CharField(max_length = 50)
+#    prereqs = models.CharField(max_length = 50)
 
-   class Meta:
-      db_table = "certs"
+#    class Meta:
+#       db_table = "certs"
 
 class Certificates(models.Model):
 
