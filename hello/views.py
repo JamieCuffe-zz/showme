c57a88afebf534457edf6e26c5e974686fc8bdff
mopeyemi <moyinoluwa@gmail.com>
2018-05-01 23:22:04 -0400

    test certificate attainable

--- hello/views.py
+++ hello/views.py
@@ -332,7 +332,7 @@ def metainfo(request):
                 completeCert += 1
             else:
                 # calculates if the certificate is attainable 
-                if totalOutput[i]["count"]/totalOutput[i]["min_needed"] >= 0.8:
+                if totalOutput[i]["count"]/totalOutput[i]["min_needed"] >= 0.2:
                     attainable += 1
                     neededCourses += totalOutput[i]["min_needed"] - totalOutput[i]["count"]
         
