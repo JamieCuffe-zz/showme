ac8642fd48e713ab375fa5985876b43201ef0d63
mopeyemi <moyinoluwa@gmail.com>
2018-05-08 19:01:35 -0400

    text color

--- hello/views.py
+++ hello/views.py
@@ -261,10 +261,15 @@ def certificate(request):
         for i in range(0, len(totalOutput)):
             colors = ["info", "danger", "success", "warning", "primary"]
             for j in range(0, len(totalOutput[i]["req_list"])):
+                textColor = "#000000"
                 percentage = 0
                 if totalOutput[i]["req_list"][j]["min_needed"] != 0:
                     percentage = totalOutput[i]["req_list"][j]["count"]/totalOutput[i]["req_list"][j]["min_needed"] * 100
-                totalOutput[i]["req_list"][j]["barGraph"] = [colors[j%5], totalOutput[i]["req_list"][j]["count"], totalOutput[i]["req_list"][j]["min_needed"], percentage]
+                if percentage > 100:
+                    percentage = 100
+                if percentage == 0:
+                    textColor = "#ffffff"
+                totalOutput[i]["req_list"][j]["barGraph"] = [colors[j%5], totalOutput[i]["req_list"][j]["count"], totalOutput[i]["req_list"][j]["min_needed"], percentage, textColor]
 
         # orders courses
         for i in range(0, len(totalOutput)):
